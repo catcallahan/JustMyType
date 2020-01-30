a6b5ec799a6c44e3d1839b8ed8c14746c60299cf
IB_Student <student@InnovateBhams-MacBook-Pro.local>
2020-01-30 11:48:22 -0600

    took out loops, works to feedback glyphs

--- scripts.js
+++ scripts.js
@@ -4,11 +4,10 @@ let sentenceCounter = 0
 let letterCounter = 0
 let m = 0
 
-// sentences[sentenceCounter][letterCounter]; // apply bg color to this 
 
 $('#keyboard-upper-container').hide();
-$('#sentence').append(sentences[sentenceCounter]);
-
+$('#sentence').text(sentences[sentenceCounter]);
+$('#target-letter').text(sentences[sentenceCounter][letterCounter]);
 
 $(document).keydown(function (e) {
     if (e.keyCode == 16) {
@@ -19,7 +18,46 @@ $(document).keydown(function (e) {
     let id = $(`#${letter}`);
     $(id).css('background-color', 'yellow');
 
-   
+})
+
+$(document).keypress(function (e) {
+    if (sentences[sentenceCounter].charCodeAt(letterCounter) == e.which) {
+        $('#target-letter').text(sentences[sentenceCounter].charAt(letterCounter + 1))
+        if(sentences[sentenceCounter].charCodeAt(letterCounter + 1)== 32){
+            $('#target-letter').text('Space')
+            $('#target-letter').css('color', 'lightgrey')
+        } else {
+            $('#target-letter').css('color', 'black')
+        }
+        letterCounter++
+        m++
+        $('#yellow-block').css({
+            'left': `${(15 * m)}px`
+        })
+
+        $('#feedback').append($('<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>'))
+        
+
+        
+    } else if(sentences[sentenceCounter].charCodeAt(letterCounter) != e.which){
+                $('#feedback').append($('<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>'))
+        
+            }
+            
+    if (letterCounter == sentences[sentenceCounter].length) {
+        $('#target-letter').empty();
+        $('#sentence').empty();
+        $('#feedback').text('')
+
+        sentenceCounter++
+        letterCounter = 0
+        $('#sentence').text(sentences[sentenceCounter]);
+        $('#yellow-block').css({
+            'left': '15px'
+        })
+        m = 0
+    };
+
 });
 
 $(document).keyup(function (e) {
@@ -32,36 +70,4 @@ $(document).keyup(function (e) {
     $(id).css('background-color', "#f5f5f5")
 });
 
-$(document).keypress(function(e){
-    for(i=0; i < sentences.length; i++){
-        for(a = 0; a<sentences[i].length; a++){
-            for(x = 0; x<sentences[i][a].length; x++)
-            
-            if(sentences[i][a].charCodeAt(x) == e.which){
-                letterCounter++
-                m++
-                let index = 
-                $('#yellow-block').css({
-                    'left' :`${(15 * m)}px`
-                })
-                $('#target-letter').append(sentences[i][a].charAt(x))
-                
-                return
-            }
-            if(letterCounter == sentences[i].length){
-                $('#target-letter').empty();
-                $('#sentence').empty();
-                sentenceCounter++
-                letterCounter = 0
-                $('#sentence').append(sentences[sentenceCounter]);
-                $('#yellow-block').css({
-                    'left' : '15px'
-                })
-                m=0
-                
-                return
-            }
-        }
-    }
-})
 

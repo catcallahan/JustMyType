bb7b89b6f4a4c2c14f47c7ba4fb7defb92c2004b
IB_Student <student@InnovateBhams-MacBook-Pro.local>
2020-01-31 13:39:17 -0600

    timer works at page load

--- scripts.js
+++ scripts.js
@@ -1,41 +1,48 @@
-// Hide Upper Keyboard on page load: //
+// -- Variables & Counters --- //
 let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
 let sentenceCounter = 0;
 let letterCounter = 0;
-let m = 0;
 let numberOfMistakes = 0;
 let numberOfWords = 0;
-
-
-
-let timerStart = 0
+let gameStart = false
 let timerEnd = 0
 let started = 0
+
+// -------------------------- On Page load----------------------//
+
 $('#keyboard-upper-container').hide();
 $('#sentence').text(sentences[sentenceCounter]);
 $('#target-letter').text(sentences[sentenceCounter][letterCounter]);
 
 
+// ---- Start Timer -----//
+let timerStart = Date.now();
+
+
+// ------ Change boards when pressing shift ------//
 $(document).keydown(function (e) {
     if (e.keyCode == 16) {
         $('#keyboard-upper-container').show();
         $('#keyboard-lower-container').hide();
     }
+    //---- Change color of pressed key on onscreen keyboard ---//
     let letter = e.key.charCodeAt(0);
     let id = $(`#${letter}`);
     $(id).css('background-color', 'yellow');
 
-})
+});
+
+// -------------------- Game Play Start -----------------------//
 
 $(document).keypress(function (e) {
+    if (gameStart = false) {
+        gameStart = true 
+    }
 
-   
-// // //     initialize a game state var in a global scope check if true, if not true then initialize to be true, then start timer at that point, when game is over take the initailize to false.
-   
-
+    // -- Check key press to correct sentence index ---//
 
     if (sentences[sentenceCounter].charCodeAt(letterCounter) == e.which) {
-        
+
         $('#target-letter').text(sentences[sentenceCounter].charAt(letterCounter + 1))
         if (sentences[sentenceCounter].charCodeAt(letterCounter + 1) == 32) {
             $('#target-letter').text('Space')
@@ -45,46 +52,51 @@ $(document).keypress(function (e) {
             $('#target-letter').css('color', 'black')
         }
         letterCounter++
-        m++
+
+        // --- Move Yellow Block along sentence when correct letter pressed ---//
         $('#yellow-block').css('left', '+=17.5px')
+
+        //--- Insert green check if correct letter ---- //
         $('#feedback').append($('<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>'))
 
-
-
+        //------- Insert x mark if not correct letter ---- //
     } else if (sentences[sentenceCounter].charCodeAt(letterCounter) != e.which) {
         $('#feedback').append($('<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>'))
         numberOfMistakes++
     }
 
+    // --- Change to another sentence when current one is completed --- //
     if (letterCounter == sentences[sentenceCounter].length) {
         $('#target-letter').empty();
         $('#sentence').empty();
         $('#feedback').text('')
-
         sentenceCounter++
         letterCounter = 0
         $('#sentence').text(sentences[sentenceCounter]);
+        
+        //--- Reset Yellow Block ---//
         $('#yellow-block').css({
             'left': '29px'
         })
-        m = 0
+        
     };
+
+    // -------------------- Game End ---------------------------------//
     if (sentenceCounter == sentences.length) {
-        // timerEnd = new Date();
-        // console.log(timerEnd)
-        // let diff = (timerEnd - timerStart);
-        // console.log(diff)
-        // let seconds = Math.floor(diff / 1000);
-        // console.log(seconds);
-        // let minutes = Math.floor(seconds / 60);
-        // console.log(minutes);
-        // let wpm = numberOfWords / minutes - 2 * numberOfMistakes;
-        // console.log(wpm)
+        // --- End timer and calculate wpm --//
+        gameStart = false;
+        timerEnd = Date.now();
+        diff = (timerEnd - timerStart)/1000
+        minutes = diff/60
+        let wpm = Math.floor((54 / minutes) - (2 * numberOfMistakes))
+        
+
+        //--- Create End page and restart button --//
         $('#keyboard-upper-container').hide();
         $('#keyboard-lower-container').hide();
         $('#32').hide();
         $('#prompt-container').empty();
-        $('.container').append('<p class = "end-message"> All Done! Play Again?</p>', '<p class ="timer">' + wpm + '</p>');
+        $('.container').append('<p class = "end-message"> All Done! Play Again?</p>');
         $('.end-message').css({
             'text-align': 'center',
             'font-size': '60px',
@@ -92,19 +104,15 @@ $(document).keypress(function (e) {
         })
         $('.container').append('<p id = "button"><button class = "play-again"> Play Again </button></p>');
         $('#button').css({
-            'text-align' : 'center',
+            'text-align': 'center',
             'font-size': '40px',
             'font-family': 'Courier New, Courier, monospace',
         })
-
-
-
-
+        $('#button').prepend('<p id = "wpm">' + wpm + '</p>')
     }
-
-
 });
 
+//--- change keys back to original color when not being pressed ---//
 $(document).keyup(function (e) {
     if (e.keyCode == 16) {
         $('#keyboard-upper-container').hide();
@@ -114,5 +122,3 @@ $(document).keyup(function (e) {
     let id = $(`#${letter}`);
     $(id).css('background-color', "#f5f5f5")
 });
-
-
